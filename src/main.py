6da14b77b9d566221211cd7e2cc4e68da0cea2e0
Thomastree <thomastree@126.com>
2026-09-03 09:16:51 -0600

    sep.3

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
-# 这是一个示例 Python 脚本。
+import math
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+x = 100
+print(abs(x))
+print(math.sqrt(x))
+print(math.exp(x))
+print(math.log(x))
+print(math.log(x,10))
+print("Muhehehe")
+
+print(2**2**3)
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+print(f"Value: {x}, Type: {type(x)}") 
